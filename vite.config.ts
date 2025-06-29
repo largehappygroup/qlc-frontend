d5910e65d505b6d0a1180ee560dafeda66738de6
draw-n <helen.wu1219@gmail.com>
2025-06-29 16:32:04 +0800

    update port to match server

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    plugins: [react()],
+    preview: {
+        port: 5703,
+        strictPort: true,
     },
-  },
-})
+    server: {
+        port: 5703,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:5703",
+    },
+    resolve: {
+        alias: {
+            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+            "@tabler/icons-react":
+                "@tabler/icons-react/dist/esm/icons/index.mjs",
+        },
+    },
+});
