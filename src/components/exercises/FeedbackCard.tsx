c8c5cb64946e585cfd3472e0ec7b59e5e3189cc8
draw-n <helen.wu1219@gmail.com>
2025-10-19 10:30:57 -0500

    distinguishing feedback from the rest

--- src/components/exercises/FeedbackCard.tsx
+++ src/components/exercises/FeedbackCard.tsx
@@ -1,35 +1,35 @@
-import { Flex, Text, Badge, Button  } from "@mantine/core";
+import { Flex, Text, Badge, Button, Card } from "@mantine/core";
 import FeedbackSliders from "./FeedbackSliders";
 
 const FeedbackCard: React.FC = () => {
     return (
-        <>
+        <Card bg="cyan" c="white">
             <Flex
                 gap="md"
                 direction={{ base: "column", sm: "row" }}
                 justify="space-between"
                 align={{ base: "start", sm: "end" }}
             >
-                <Flex direction="column" justify="flex-start">
+                <Text size="xl" fw="bold">
+                    Please Provide Feedback
+                </Text>
+                <Flex direction="column" justify="space-between" align="end">
                     <Badge variant="default" size="md">
                         Not Started
                     </Badge>
-                    <Text size="xl" fw="bold">
-                        Please Provide Feedback
-                    </Text>
+                    <FeedbackSliders>
+                        <Button
+                            radius="xl"
+                            size="sm"
+                            color="white"
+                            w={{ base: "100%", lg: "auto" }}
+                        >
+                            Give Feedback
+                        </Button>
+                    </FeedbackSliders>
                 </Flex>
-
-                <FeedbackSliders>
-                    <Button
-                        radius="xl"
-                        size="sm"
-                        w={{ base: "100%", lg: "auto" }}
-                    >
-                        Give Feedback
-                    </Button>
-                </FeedbackSliders>
             </Flex>
-        </>
+        </Card>
     );
 };
 
