d3a724e39754373f2e8244beb24b452ae8b87aca
draw-n <helen.wu1219@gmail.com>
2026-01-03 11:27:17 -0500

    add query provider

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,18 @@ import theme from "./theme.ts";
 import App from "./App.tsx";
 import { AuthProvider } from "./hooks/AuthContext.tsx";
 import { MantineProvider } from "@mantine/core";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <AuthProvider>
-            <MantineProvider theme={theme}>
-                <App />
-            </MantineProvider>
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <MantineProvider theme={theme}>
+                    <App />
+                </MantineProvider>
+            </AuthProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
