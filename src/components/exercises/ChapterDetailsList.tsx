204970491d753a210cd3806218adf5e0ebb56037
draw-n <helen.wu1219@gmail.com>
2026-02-25 15:03:34 -0600

    hide chapters if not released yet

--- src/components/exercises/ChapterDetailsList.tsx
+++ src/components/exercises/ChapterDetailsList.tsx
@@ -3,22 +3,35 @@ import ExerciseCard from "./ExerciseCard";
 import FeedbackCard from "./FeedbackCard";
 import { WithChapter } from "../../types/Chapter";
 import { useAllAssignments } from "../../hooks/useAssignments";
-
-
+import { useAllChapters } from "../../hooks/useChapters";
 
 const ChapterDetailsList: React.FC<WithChapter> = ({
     chapter,
 }: WithChapter) => {
-
     if (chapter && !chapter?.released) {
         return <Text>No exercises available.</Text>;
     }
-
+    const { data: chapters } = useAllChapters();
     const { data: chapterAssignments, isLoading } = useAllAssignments(
         chapter?.uuid,
     );
 
-    const items = chapterAssignments?.map((assignment, index) => (
+    let assignments;
+
+    if (!chapter) {
+        // allChapters is already retrieved
+        assignments = chapterAssignments?.filter((assignment) =>
+            chapters?.some(
+                (chap) =>
+                    chap.uuid === assignment.chapterId &&
+                    chap.released === true,
+            ),
+        );
+    } else {
+        assignments = chapterAssignments;
+    }
+
+    const items = assignments?.map((assignment, index) => (
         <ExerciseCard index={index} assignment={assignment} />
     ));
 
@@ -27,7 +40,9 @@ const ChapterDetailsList: React.FC<WithChapter> = ({
             {isLoading ? (
                 <Loader type="dots" size="xl" />
             ) : !items || items?.length === 0 ? (
-                <Text style={{marginBottom: "10px"}}>No exercises found.</Text>
+                <Text style={{ marginBottom: "10px" }}>
+                    No exercises found.
+                </Text>
             ) : (
                 chapter?.requestFeedback && (
                     <FeedbackCard chapterId={chapter?.uuid} />
